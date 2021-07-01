5e65c1c9f55ef6c58b96bee33abba791f61d2718
jeewantha upul <59133288+jeewantha-upul@users.noreply.github.com>
2021-07-01 11:09:02 +0530

    ui finished

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,39 +12,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 100,
-                color: Colors.red,
-                child: Text('1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/my.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                    child: Text('1'),
+              SizedBox(height: 10.0,),
+              Text(
+                'jeewantha upul',
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ), SizedBox(height: 10.0,),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: "Source Sans",
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ), SizedBox(height: 10.0,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0 ,horizontal: 16.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '077-63-81-82-4',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans',
+                        fontSize: 20.0,
+                        color: Colors.teal[900]
+                      ),
+                    ),
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    child: Text('1'),
-                  )
-                ],
+                ),
               ),
-              Container(
-                width: 100,
-                color: Colors.blue,
-                child: Text('1'),
-              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0 ,horizontal: 16.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'Upuljeewantha070@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans',
+                          fontSize: 20.0,
+                          color: Colors.teal[900]
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
-          )  ,
+          ),
         ),
       ),
     );
